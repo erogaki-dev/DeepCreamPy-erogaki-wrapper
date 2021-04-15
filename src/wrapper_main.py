dcf7e4b7499598e86ba9f6522dfdf554c5d0be59
Yurifag <yurifag@grzb.de>
2021-04-15 17:32:07 +0200

    Add functions for image conversion
    Add functions to convert between encoded images and PIL Image objects.

--- src/wrapper_main.py
+++ src/wrapper_main.py
@@ -11,6 +11,18 @@ from PIL import Image
 import redis
 import io
 
+# convert encoded image file to PIL image object
+def bytes_to_image(bytes):
+    img_file = io.BytesIO(bytes)
+    img_file.seek(0)
+    return Image.open(img_file)
+
+# convert PIL image object to PNG
+def image_to_bytes(image):
+    img_file = io.BytesIO()
+    image.save(img_file, format="PNG")
+    return img_file.getvalue()
+
 def main():
     r = redis.Redis(host="localhost", port=6379, db=0)
 
@@ -20,24 +32,17 @@ def main():
     decensor_instance = Decensor()
     decensor_instance.load_model()
 
-
     while True:
         print("ready to receive censored images")
         _, uuid = r.blpop("censored-images:deepcreampy:bar", 0)
-        print("ready to receive censored images")
+        print("received censored image")
 
         censored_img_data = r.get("censored-images:%s" % uuid.decode())
+        censored_img = bytes_to_image(censored_img_data)
 
-        censored_img_file = io.BytesIO(censored_img_data)
-        censored_img_file.seek(0)
-
-        censored_img = Image.open(censored_img_file)
         decensored_img = decensor_instance.decensor_image_variation(censored_img, censored_img, 0)
 
-        decensored_img_file = io.BytesIO()
-        decensored_img.save(decensored_img_file, format="PNG")
-
-        r.set("decensored-images:%s" % uuid.decode(), decensored_img_file.getvalue())
+        r.set("decensored-images:%s" % uuid.decode(), image_to_bytes(decensored_img))
 
 if __name__ == "__main__":
     main()
